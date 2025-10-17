f6ac7a5f5a758aeb4222433a3a34211bd1a220bc
cyril <145072652+Codes004@users.noreply.github.com>
2025-10-17 02:12:26 +0100

    modifyed the code

--- admin_dashboard/admin.py
+++ admin_dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 
-# Register your models here.
+# Override admin homepage
+admin.site.index_template = None
+
+def custom_admin_index(request):
+    return redirect("admin_dashboard")
+
+admin.site.index = custom_admin_index
