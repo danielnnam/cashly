6b8dcea1c66a820d6cf9092fcb6fd1456e66d2ed
cyril <145072652+Codes004@users.noreply.github.com>
2025-10-17 02:07:21 +0100

    Created a new user registred signal

--- accounts/signals.py
+++ accounts/signals.py
@@ -5,7 +5,7 @@ from .models import Notification, Profile
 from .models import LoginActivity
 from django.utils import timezone
 from django.contrib.auth.signals import user_logged_in
-
+from admin_dashboard.utils import log_activity
 
 @receiver(post_save, sender=User)
 def create_or_update_profile(sender, instance, created, **kwargs):
@@ -39,3 +39,21 @@ def get_client_ip(request):
     if x_forwarded_for:
         return x_forwarded_for.split(",")[0]
     return request.META.get("REMOTE_ADDR")
+
+
+
+@receiver(post_save, sender=User)
+def new_user_registered(sender, instance, created, **kwargs):
+    """Logs new user registrations to admin activity dashboard."""
+    if created:
+        # You can detect if it’s a Google user via allauth if needed
+        login_method = "Google" if instance.socialaccount_set.exists() else "Email"
+
+        log_activity(
+            user=instance,
+            title="New User Registration",
+            description=f"User #{instance.id} ({instance.username}) registered via {login_method}",
+            type="user_registration",
+            status="completed",
+            icon="user-plus"
+        )
