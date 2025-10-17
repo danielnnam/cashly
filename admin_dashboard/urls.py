7c10a5b6918113f44bb897f49698113bd3d90df8
cyril <145072652+Codes004@users.noreply.github.com>
2025-10-17 02:10:42 +0100

    Modifyed the file

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = "admin_dashboard"
+
 urlpatterns = [
-    path('', views.admin_dashboard, name='admin_dashboard'),
-    path('login/', views.admin_login, name='admin_login'),
+    path('dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('chart-data/', views.transaction_chart_data, name='transaction_chart_data'),
+    path("activity-data/", views.recent_activity, name="recent_activity"),
+    path('users/', views.users_list, name='users'),
+    path("users/<int:user_id>/", views.user_detail, name="user_detail"),
+    path('user/<int:user_id>/suspend/', views.suspend_user, name='admin_suspend_user'),
+    path('user/<int:user_id>/delete/', views.delete_user, name='admin_delete_user'),
+    
 ]
