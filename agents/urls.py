ee08e389b627b09480c8039abf5373cd3788df4b
cyril <145072652+Codes004@users.noreply.github.com>
2025-10-22 17:03:45 +0100

    Suspension code url

--- agents/urls.py
+++ agents/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("become-agent/", views.become_agent, name="become_agent"),
     path("agent_wallet/", views.agent_wallet, name="agent_wallet"),
     path('history/', views.agent_history, name='agent_history'),
+    path('suspended/', views.suspension_notice, name='suspension_notice'),
+    path('appeal-suspension/', views.appeal_suspension, name='appeal_suspension'),
     path('logout/', views.logout_view, name='logout'), 
 ]
