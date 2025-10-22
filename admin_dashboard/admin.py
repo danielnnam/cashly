9281953369a227aec2b439a89cb8185ff7ee55b7
cyril <145072652+Codes004@users.noreply.github.com>
2025-10-22 18:07:45 +0100

    Updated the admin codes

--- admin_dashboard/admin.py
+++ admin_dashboard/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.shortcuts import redirect
+from wallet.models import Transaction
 
 # Override admin homepage
 admin.site.index_template = None
@@ -8,3 +9,9 @@ def custom_admin_index(request):
     return redirect("admin_dashboard")
 
 admin.site.index = custom_admin_index
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('reference', 'wallet', 'transaction_type', 'amount', 'status', 'created_at')
+    search_fields = ('reference', 'wallet__user__username')
+    list_filter = ('transaction_type', 'status', 'created_at')
